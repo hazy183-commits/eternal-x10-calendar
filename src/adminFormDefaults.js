8c3a07b7476639f7c1c57f21b9af344bbf61e50f
KiRY <hazy183@gmail.com>
2026-09-15 04:41:10 +0200

    Set all raid bosses as Epic RB

--- src/adminFormDefaults.js
+++ src/adminFormDefaults.js
@@ -1,9 +1,9 @@
 export const BOSS_PRESETS = Object.freeze({
-  'Queen Ant': Object.freeze({ slug: 'queen-ant', location: 'Queen Ant Nest', type: 'RB' }),
-  Core: Object.freeze({ slug: 'core', location: 'Core Chamber', type: 'RB' }),
-  Orfen: Object.freeze({ slug: 'orfen', location: 'Sea of Spores', type: 'RB' }),
+  'Queen Ant': Object.freeze({ slug: 'queen-ant', location: 'Queen Ant Nest', type: 'Epic RB' }),
+  Core: Object.freeze({ slug: 'core', location: 'Core Chamber', type: 'Epic RB' }),
+  Orfen: Object.freeze({ slug: 'orfen', location: 'Sea of Spores', type: 'Epic RB' }),
   Baium: Object.freeze({ slug: 'baium', location: "Baium's Tower", type: 'Epic RB' }),
-  Zaken: Object.freeze({ slug: 'zaken', location: "Devil's Isle", type: 'RB' }),
+  Zaken: Object.freeze({ slug: 'zaken', location: "Devil's Isle", type: 'Epic RB' }),
   Frintezza: Object.freeze({ slug: 'frintezza', location: "Frintezza's Hall", type: 'Epic RB' }),
   Antharas: Object.freeze({ slug: 'antharas', location: "Antharas' Lair", type: 'Epic RB' }),
   Valakas: Object.freeze({ slug: 'valakas', location: "Valakas' Lair", type: 'Epic RB' }),
