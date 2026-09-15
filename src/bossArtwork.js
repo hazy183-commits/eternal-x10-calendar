9662d1a714c7e8ed01743347117056d120dd8608
hazy183-commits <hazy183@gmail.com>
2026-09-15 10:08:17 +0200

    Enable new admin start screen

--- src/bossArtwork.js
+++ src/bossArtwork.js
@@ -2,6 +2,7 @@ import './landingPremium.css';
 import './landingRecruitHighlight.css';
 import './landingPremium.js';
 import './bossDetailsPl.js';
+import './adminHomeScreen.js';
 
 // Vite serves public/ directly and copies it unchanged into production builds.
 const artworkBase = `${import.meta.env.BASE_URL}images/bosses/`;
